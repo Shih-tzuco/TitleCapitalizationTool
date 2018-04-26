50a2644eee4d04b8840badcf833c160deb2c33c7
Hanna <annuschk@gmail.com>
2018-04-26 15:51:51 +0300

    null check was changed

--- TitleCapitalizationTools/Program.cs
+++ TitleCapitalizationTools/Program.cs
@@ -15,26 +15,24 @@ namespace TitleCapitalizationTool
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 string text = Console.ReadLine();
 
-                if (text.Length != 0)
+                text = text.Trim();
+                string[] preposition = new string[] { "A", "An", "At", "But", "By", "For", "In", "Nor", "On", "Or", "Out", "So", "The", "To", "Up", "Yet" };
+                while (text.Contains("  "))
                 {
-                    text = text.Trim();
-                    string[] preposition = new string[] { "A", "An", "At", "But", "By", "For", "In", "Nor", "On", "Or", "Out", "So", "The", "To", "Up", "Yet" };
-                    while (text.Contains("  "))
+                    text = text.Replace("  ", " ");
+                }
+                string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+                string[] split = titleCase.Split(new char[] { ' ' });
+                for (int i = 0; i < split.Length; ++i)
+                {
+                    foreach (string newSplit in preposition)
                     {
-                        text = text.Replace("  ", " ");
-                    }
-                    string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
-                    string[] split = titleCase.Split(new char[] { ' ' });
-                    for (int i = 0; i < split.Length; ++i)
-                    {
-                        foreach (string newSplit in preposition)
+                        if (split[i].Equals(newSplit))
                         {
-                            if (split[i].Equals(newSplit))
-                            {
-                                split[i] = split[i].ToLower();
-                            }
+                            split[i] = split[i].ToLower();
                         }
                     }
+                }
                 titleCase = string.Join(" ", split);
                 StringBuilder builder = new StringBuilder();
 
@@ -66,12 +64,14 @@ namespace TitleCapitalizationTool
                         i++;
                     }
                 }
+                if (builder.Length != 0)
+                {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.Write("Output String: ");
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine(builder);
                 Console.Write("\n");
-            }
+                }
         }
             while (true);
         }
