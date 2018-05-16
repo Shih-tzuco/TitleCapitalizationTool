6e933373c51bb4071ba34e9a924b982750aae593
Hanna <annuschk@gmail.com>
2018-05-16 15:00:41 +0300

    changed when just whitespace input in string

--- TitleCapitalizationTools/MainClass.cs
+++ TitleCapitalizationTools/MainClass.cs
@@ -6,80 +6,98 @@ namespace TitleCapitalizationTool
 {
 	public class MainClass
 	{
-		public static void Main()
+		public static StringBuilder Capitalization(string text)
 		{
-			do
-			{
-				Console.ForegroundColor = ConsoleColor.Gray;
-				Console.Write("Enter title to capitalize: ");
-				Console.ForegroundColor = ConsoleColor.Red;
-				string text = Console.ReadLine();
-				text = text.Trim();
-				while (text.Contains("  "))
-				{
-					text = text.Replace("  ", " ");
-				}
-				text = CultureInfo.CurrentCulture.TextInfo.ToLower(text);
-				string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
-				string[] separator = titleCase.Split(new char[] { ' ' });
-				string[] prepositions = { "A", "An", "At", "But", "By", "For", "In", "Nor", "Of", "On", "Or", "Out", "So", "The", "To", "Up", "Yet" };
+			text = text.Trim();
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            text = CultureInfo.CurrentCulture.TextInfo.ToLower(text);
+            string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+            string[] separator = titleCase.Split(new char[] { ' ' });
+            string[] prepositions = { "A", "An", "At", "But", "By", "For", "In", "Nor", "Of", "On", "Or", "Out", "So", "The", "To", "Up", "Yet" };
 
-				for (int i = 0; i < separator.Length; ++i)
+            for (int i = 0; i < separator.Length; ++i)
+            {
+                foreach (string newSeparator in prepositions)
+                {
+                    if (separator[i].Equals(newSeparator) && i != 0 && i != separator.Length - 1)
+                    {
+                        separator[i] = separator[i].ToLower();
+                    }
+                }
+            }
+            titleCase = string.Join(" ", separator);
+            StringBuilder builder = new StringBuilder();
+            char[] punctuation = { ',', ':', ';', '?', '.', '!', '-' };
+            for (int i = 0; i < titleCase.Length; ++i)
+            {
+                bool space = false;
+                for (int j = 0; j < punctuation.Length; ++j)
+                {
+                    if (titleCase[i] == ' ' && titleCase[i + 1] == punctuation[j])
+                    {
+                        i++;
+                    }
+                    if (titleCase[i] == punctuation[j] && i + 1 != titleCase.Length)
+                    {
+                        space = true;
+                    }
+                }
+                if (titleCase[i] == '-')
+                {
+                    builder.Append(' ');
+                }
+                builder.Append(titleCase[i]);
+                if (space)
+                {
+					if (i == ' ' && i + 1 != ' ')
+                    {
+                        builder.Append(' ');
+                        builder.Append(titleCase[i + 1].ToString().ToUpper());
+                        i++;
+                    }
+                }
+            }
+			return builder;
+		}
+		public static void Main(string[] text)
+		{
+			if (text.Length != 0)
 				{
-					foreach (string newSeparator in prepositions)
+					for (int i = 0; i < text.Length; ++i)
 					{
-						if (separator[i].Equals(newSeparator) && i != 0 && i != separator.Length - 1)
-						{
-							separator[i] = separator[i].ToLower();
-						}
+						Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("Enter title to capitalize: ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write("Capitalized title: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Capitalization(text[i]));
+                        Console.Write("\n");
 					}
 				}
-				titleCase = string.Join(" ", separator);
-				StringBuilder builder = new StringBuilder();
-				char[] punctuation = { ',', ':', ';', '?', '.', '!', '-' };
-				for (int i = 0; i < titleCase.Length; ++i)
-				{
-					bool space = false;
-					for (int j = 0; j < punctuation.Length; ++j)
-					{
-						if (titleCase[i] == ' ' && titleCase[i + 1] == punctuation[j])
-						{
-							i++;
-						}                  
-						if (titleCase[i] == punctuation[j] && i + 1 != titleCase.Length)
-						{
-							space = true;
-						}
-					}
-					if (titleCase[i] == '-')
-					{
-						builder.Append(' ');
-					}
-					builder.Append(titleCase[i]);
-					if (space)
-					{
-						if (i == ' ' && i + 1 != ' ')
-						{
-							builder.Append(' ');
-                            builder.Append(titleCase[i + 1].ToString().ToUpper());
-							i++;
-						}
-					}
-				}
-				if (builder.Length != 0)
+			else 
+			{
+				while (true)
 				{
 					Console.ForegroundColor = ConsoleColor.Gray;
-					Console.Write("Capitalized title: ");
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine(builder);
-					Console.Write("\n");
-				}
-				else
-				{
-					Console.SetCursorPosition(0, Console.CursorTop - 1);
-				}
+                    Console.Write("Enter title to capitalize: ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    string newText = Console.ReadLine();
+					while (newText.Length == 0)
+					{
+						Console.SetCursorPosition(28, Console.CursorTop - 1);
+						newText = Console.ReadLine();
+					}
+					Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write("Capitalized title: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Capitalization(newText));
+                    Console.Write("\n");
+				}            
 			}
-			while (true);
 		}
 	}
 }
